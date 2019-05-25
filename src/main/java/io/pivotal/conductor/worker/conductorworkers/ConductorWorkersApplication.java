0796c5730509f0edf578b3be7e3909910c8bde0a
David Wadden <dwadden@pivotal.io>
2019-05-25 13:18:42 -0500

    Bootstrap project from Spring Initializr

---
+++ src/main/java/io/pivotal/conductor/worker/conductorworkers/ConductorWorkersApplication.java
@@ -0,0 +1,13 @@
+package io.pivotal.conductor.worker.conductorworkers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConductorWorkersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConductorWorkersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/pivotal/conductor/worker/conductorworkers/ConductorWorkersApplicationTests.java
@@ -0,0 +1,16 @@
+package io.pivotal.conductor.worker.conductorworkers;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConductorWorkersApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
