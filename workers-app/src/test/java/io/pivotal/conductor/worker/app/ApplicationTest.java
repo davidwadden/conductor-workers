e67b21bf8c05b411724858f1c17f857926c5f3ab
David Wadden <dwadden@pivotal.io>
2019-06-15 16:04:30 -0500

    Mock taskCoordinator bean for contextLoads test

--- workers-app/src/test/java/io/pivotal/conductor/worker/app/ApplicationTest.java
+++ workers-app/src/test/java/io/pivotal/conductor/worker/app/ApplicationTest.java
@@ -1,13 +1,18 @@
 package io.pivotal.conductor.worker.app;
 
+import com.netflix.conductor.client.task.WorkflowTaskCoordinator;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 class ApplicationTest {
+
+    @MockBean
+    private WorkflowTaskCoordinator noOpTaskCoordinator;
 
     @Test
     void contextLoads() {
