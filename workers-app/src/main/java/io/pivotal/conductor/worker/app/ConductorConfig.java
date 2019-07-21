bcbdc2ba91ee5b4b0c5f182929aba07258382a46
David Wadden <dwadden@pivotal.io>
2019-07-21 14:24:48 -0500

    Support Java 8 compilation
      + Use factory bean methods for property binding

--- workers-app/src/main/java/io/pivotal/conductor/worker/app/ConductorConfig.java
+++ workers-app/src/main/java/io/pivotal/conductor/worker/app/ConductorConfig.java
@@ -4,17 +4,8 @@ import com.netflix.conductor.client.http.TaskClient;
 import com.netflix.conductor.client.task.WorkflowTaskCoordinator;
 import com.netflix.conductor.client.task.WorkflowTaskCoordinator.Builder;
 import com.netflix.conductor.client.worker.Worker;
-import io.pivotal.conductor.worker.app.ConductorConfig.ImportBitbucketWorkerConfig.PropertyBindingBitbucketProperties;
-import io.pivotal.conductor.worker.app.ConductorConfig.ImportCloudFoundryWorkerConfig.PropertyBindingCloudFoundryProperties;
-import io.pivotal.conductor.worker.app.ConductorConfig.ImportConcourseWorkerConfig.PropertyBindingConcourseProperties;
-import io.pivotal.conductor.worker.app.ConductorConfig.ImportGitHubWorkerConfig.PropertyBindingGitHubProperties;
-import io.pivotal.conductor.worker.app.ConductorConfig.ImportJiraWorkerConfig.PropertyBindingJiraProperties;
-import io.pivotal.conductor.worker.app.ConductorConfig.ImportTemplateWorkerConfig.PropertyBindingTemplateProperties;
-import io.pivotal.conductor.worker.app.ConductorConfig.ImportTrackerWorkerConfig.PropertyBindingTrackerProperties;
 import io.pivotal.conductor.worker.bitbucket.BitbucketProperties;
 import io.pivotal.conductor.worker.bitbucket.BitbucketWorkerConfig;
-import io.pivotal.conductor.worker.template.TemplateProperties;
-import io.pivotal.conductor.worker.template.TemplateWorkerConfig;
 import io.pivotal.conductor.worker.cloudfoundry.CloudFoundryProperties;
 import io.pivotal.conductor.worker.cloudfoundry.CloudFoundryWorkerConfig;
 import io.pivotal.conductor.worker.concourse.ConcourseProperties;
@@ -23,6 +14,8 @@ import io.pivotal.conductor.worker.github.GitHubProperties;
 import io.pivotal.conductor.worker.github.GitHubWorkerConfig;
 import io.pivotal.conductor.worker.jira.JiraProperties;
 import io.pivotal.conductor.worker.jira.JiraWorkerConfig;
+import io.pivotal.conductor.worker.template.TemplateProperties;
+import io.pivotal.conductor.worker.template.TemplateWorkerConfig;
 import io.pivotal.conductor.worker.tracker.TrackerProperties;
 import io.pivotal.conductor.worker.tracker.TrackerWorkerConfig;
 import java.util.Collection;
@@ -40,67 +33,60 @@ public class ConductorConfig {
     @Autowired
     private ConductorProperties properties;
 
-    @EnableConfigurationProperties(PropertyBindingBitbucketProperties.class)
-    @Import(BitbucketWorkerConfig.class)
+    @Import({
+        BitbucketWorkerConfig.class,
+        CloudFoundryWorkerConfig.class,
+        ConcourseWorkerConfig.class,
+        GitHubWorkerConfig.class,
+        JiraWorkerConfig.class,
+        TemplateWorkerConfig.class,
+        TrackerWorkerConfig.class,
+    })
     @Configuration
-    static class ImportBitbucketWorkerConfig {
+    static class ImportConfig {
 
-        @ConfigurationProperties("portal.bitbucket")
-        static class PropertyBindingBitbucketProperties extends BitbucketProperties {}
     }
 
-    @EnableConfigurationProperties(PropertyBindingCloudFoundryProperties.class)
-    @Import(CloudFoundryWorkerConfig.class)
-    @Configuration
-    static class ImportCloudFoundryWorkerConfig {
-
-        @ConfigurationProperties("portal.cloudfoundry")
-        static class PropertyBindingCloudFoundryProperties extends CloudFoundryProperties {}
+    @Bean
+    @ConfigurationProperties("portal.bitbucket")
+    public BitbucketProperties bitbucketProperties() {
+        return new BitbucketProperties();
     }
 
-    @EnableConfigurationProperties(PropertyBindingConcourseProperties.class)
-    @Import(ConcourseWorkerConfig.class)
-    @Configuration
-    static class ImportConcourseWorkerConfig {
-
-        @ConfigurationProperties("portal.concourse")
-        static class PropertyBindingConcourseProperties extends ConcourseProperties {}
+    @Bean
+    @ConfigurationProperties("portal.cloudfoundry")
+    public CloudFoundryProperties cloudFoundryProperties() {
+        return new CloudFoundryProperties();
     }
 
-    @EnableConfigurationProperties(PropertyBindingGitHubProperties.class)
-    @Import(GitHubWorkerConfig.class)
-    @Configuration
-    static class ImportGitHubWorkerConfig {
-
-        @ConfigurationProperties("portal.github")
-        static class PropertyBindingGitHubProperties extends GitHubProperties {}
+    @Bean
+    @ConfigurationProperties("portal.concourse")
+    public ConcourseProperties concourseProperties() {
+        return new ConcourseProperties();
     }
 
-    @EnableConfigurationProperties(PropertyBindingJiraProperties.class)
-    @Import(JiraWorkerConfig.class)
-    @Configuration
-    static class ImportJiraWorkerConfig {
-
-        @ConfigurationProperties("portal.jira")
-        static class PropertyBindingJiraProperties extends JiraProperties {}
+    @Bean
+    @ConfigurationProperties("portal.github")
+    public GitHubProperties gitHubProperties() {
+        return new GitHubProperties();
     }
 
-    @EnableConfigurationProperties(PropertyBindingTemplateProperties.class)
-    @Import(TemplateWorkerConfig.class)
-    @Configuration
-    static class ImportTemplateWorkerConfig {
-
-        @ConfigurationProperties("portal.template")
-        static class PropertyBindingTemplateProperties extends TemplateProperties {}
+    @Bean
+    @ConfigurationProperties("portal.jira")
+    public JiraProperties jiraProperties() {
+        return new JiraProperties();
     }
 
-    @EnableConfigurationProperties(PropertyBindingTrackerProperties.class)
-    @Import(TrackerWorkerConfig.class)
-    @Configuration
-    static class ImportTrackerWorkerConfig {
+    @Bean
+    @ConfigurationProperties("portal.template")
+    public TemplateProperties  templateProperties() {
+        return new TemplateProperties();
+    }
 
-        @ConfigurationProperties("portal.tracker")
-        static class PropertyBindingTrackerProperties extends TrackerProperties {}
+    @Bean
+    @ConfigurationProperties("portal.tracker")
+    public TrackerProperties  trackerProperties() {
+        return new TrackerProperties();
     }
 
     @Bean
